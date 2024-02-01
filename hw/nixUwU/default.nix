2a29409a7ffab2070d286818f41d4e96cb692223
Moxie Benavides <ethanpbenavides@gmail.com>
2024-01-31 20:08:26 -0600

    cleanup messy config update flake

--- hw/nixUwU/default.nix
+++ hw/nixUwU/default.nix
@@ -1,20 +1,20 @@
 { ... }:
 {
-    imports = [
-        ./boot.nix
-        ./disks.nix
-        ./hardware.nix
-        ./network.nix
-        ../../common
-    ];
-    environment.sessionVariables = {
+  imports = [
+      ./boot.nix
+      ./disks.nix
+      ./hardware.nix
+      ./network.nix
+      ../../common
+  ];
+  environment.sessionVariables = {
 		LIBVA_DRIVER_NAME = "nvidia";
 		GBM_BACKEND = "nvidia-drm";
-		__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-		VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
+		#__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+		#VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
 		WLR_NO_HARDWARE_CURSORS = "1";
-    };
-    systemd.services = {
+  };
+  systemd.services = {
 		"nv-power-limit" = {
 			enable = true;
 			description = "set nvidia gpu power to max";
@@ -24,6 +24,6 @@
   			'';
 			wantedBy = [ "multi-user.target" ];
 		};
-    };
-    system.stateVersion = "23.05";
+  };
+  system.stateVersion = "23.05";
 }
