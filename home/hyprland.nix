07496a5105c5428367ebf339fe0638cc6f9b949e
Moxie <astronomicalgamer5@gmail.com>
2024-02-14 00:41:04 -0600

    start more themeing

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -14,19 +14,55 @@
 			};
 			settings = [
 				{
-					output = [ "eDP-1" "DP-1"];
+					output = [ "DP-1"];
 					modules-left = [ "hyprland/workspaces" ];
 					modules-center = [ "hyprland/window" ];
-					modules-right = [ "wireplumber" "memory" "disk" "cpu" "temperature" "backlight" "battery" "clock" "tray" ];
+					modules-right = [ "wireplumber" "memory" "disk" "cpu" "temperature" "clock" "tray" ];
 					"hyprland/workspaces" = { format = "{name}"; };
 					"hyprland/window" = {
 						format = "{title}";
-    				max-length = 50;
-    				rewrite = {
+    					max-length = 50;
+    					rewrite = {
 							"(.*) — Mozilla Firefox" = "🌎 $1";
-			      	"vim (.*)" = " $1";
+			      			"vim (.*)" = " $1";
 							"sudo vim (.*)" = " $1";
-    				};
+    					};
+					};
+					"wireplumber" = {
+						format-muted = "muted";
+						on-click = "pavucontrol";
+					};
+					"memory" = { format = "RAM: {percentage}%";	};
+					"disk" = { format = "{percentage_free}% remaining on /";};
+					"cpu" = { format = "CPU: {usage}%"; };
+					"backlight" = {
+						format = "{percent}% {icon}";
+						format-icons = ["" ""];
+					};
+					"battery" = {
+						format = "{capacity}%";
+						format-icons = ["" "" "" "" ""];
+					};
+					"tray" = {
+						icon-size = 21;
+						show-passive-items = true;
+						spacing = 5;
+					};
+				}
+				{
+					output = [ "eDP-1" ];
+					modules-left = [ "hyprland/workspaces" ];
+					modules-center = [ "hyprland/window" ];
+					modules-right = [ "wireplumber" "memory" "cpu" "temperature" "backlight" "battery" "clock" "tray" ];
+					"hyprland/workspaces" = { format = "{name}"; };
+					"hyprland/window" = {
+						format = "{title}";
+    					max-length = 50;
+    					rewrite = {
+							"(.*) — Mozilla Firefox" = "🌎 $1";
+			      			"vim (.*)" = " $1";
+							"sudo vim (.*)" = " $1";
+    					};
 					};
 					"wireplumber" = {
 						format-muted = "muted";
@@ -89,42 +125,42 @@
 			];
 			style = ''
 				* {
-    			border:        none;
-    			border-radius: 0;
-    			font-size:     17px;
-    			font-weight:     bold;
-    			box-shadow:    none;
-    			text-shadow:   none;
-    			transition-duration: 0s;
+    				border:        none;
+    				border-radius: 0;
+    				font-size:     17px;
+    				font-weight:     bold;
+    				box-shadow:    none;
+    				text-shadow:   none;
+    				transition-duration: 0s;
+				}
+				window.eDP-1 * {
+					font-size: 12px;
 				}
 				window#waybar {
-					margin-top: 4px;
-    			background: transparent;
+    				background: transparent;
 				}
 				#workspaces {
-					padding: 5px;
-    			border-radius: 20px;
-    			background: rgba(30, 30, 46, 0.8);
-    			transition: none;
+					padding: 1px;
+    				border-radius: 20px;
+    				background: rgba(30, 30, 46, 0.8);
+    				transition: none;
 				}
 				#workspaces button {
-    			transition: none;
-    			color:      rgba(205, 214, 244, 0.8);
-    			background: transparent;
+    				transition: none;
+    				color:      rgba(205, 214, 244, 0.8);
+    				background: transparent;
 				}
 				#workspaces button.active {
-					border:        none;
 					border-radius: 20px;
 					color:  	#181825;
 					background: #f5c2e7;
 				}
 				#workspaces button:hover {
-    			border: 3px solid #cba6f7;
 					border-radius: 20px;
-    			transition: none;
-    			box-shadow: inherit;
-    			text-shadow: inherit;
-    			color: #cba6f7;
+    				transition: none;
+    				box-shadow: inherit;
+    				text-shadow: inherit;
+    				color: #cba6f7;
 				}
 				#workspaces button.urgent {
     			color:      rgba(180, 190, 254, 1);
@@ -132,80 +168,82 @@
 
 				#window {
 					border-radius: 20px;
-					padding-left: 10px;
-    			padding-right: 10px;
 					background: rgba(30, 30, 46, 0.85);
 				}
 
 				#clock {
 					margin-left: 4px;
-    			margin-right: 4px;
+    				margin-right: 4px;
 					border-radius: 20px;
 					padding-left: 10px;
-    			padding-right: 10px;
+    				padding-right: 10px;
 					background: rgba(30, 30, 46, 0.85);
+				}
+
+				#battery {
+					
 				}
 
 				#cpu {
 					margin-left: 4px;
-    			margin-right: 4px;
+    				margin-right: 4px;
 					border-radius: 20px;
 					padding-left: 10px;
-    			padding-right: 10px;
+    				padding-right: 10px;
 					background: rgba(30, 30, 46, 0.85);
 				}
 
 				#disk {
 					margin-left: 4px;
-    			margin-right: 4px;
+    				margin-right: 4px;
 					border-radius: 20px;
 					padding-left: 10px;
-    			padding-right: 10px;
+    				padding-right: 10px;
 					background: rgba(30, 30, 46, 0.85);
 				}
 
 				#memory {
 					margin-left: 4px;
-    			margin-right: 4px;
+    				margin-right: 4px;
 					border-radius: 20px;
 					padding-left: 10px;
-    			padding-right: 10px;
+    				padding-right: 10px;
 					background: rgba(30, 30, 46, 0.85);
 				}
 
 				#network {
 					margin-left: 4px;
-    			margin-right: 4px;
+    				margin-right: 4px;
 					border-radius: 20px;
 					padding-left: 10px;
-    			padding-right: 10px;
+    				padding-right: 10px;
 					background: rgba(30, 30, 46, 0.85);
 				}
 
 				#tray {
 					margin-left: 4px;
-    			margin-right: 4px;
+    				margin-right: 4px;
 					border-radius: 20px;
 					padding-left: 10px;
-    			padding-right: 10px;
+    				padding-right: 10px;
 					background: rgba(30, 30, 46, 0.85);
 				}
 
 				#temperature {
 					margin-left: 4px;
-    			margin-right: 4px;
+    				margin-right: 4px;
 					border-radius: 20px;
 					padding-left: 10px;
-    			padding-right: 10px;
+    				padding-right: 10px;
 					background: rgba(30, 30, 46, 0.85);
 				}
 
 				#wireplumber {
 					margin-left: 4px;
-    			margin-right: 4px;
+    				margin-right: 4px;
 					border-radius: 20px;
 					padding-left: 10px;
-    			padding-right: 10px;
+    				padding-right: 10px;
 					background: rgba(30, 30, 46, 0.85);
 				}
 			'';
