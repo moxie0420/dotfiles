76ad6279f86cccaf25b42a922f4b810f79c0b4bf
Moxie <astronomicalgamer5@gmail.com>
2024-02-13 23:32:16 -0600

    update hyprland config

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -161,20 +161,20 @@
 				",XF86AudioNext, exec, ${pkgs.playerctl}/bin/playerctl next"
 				",XF86AudioPrev, exec, ${pkgs.playerctl}/bin/playerctl previous"
 			] ++ (
-      # workspaces
-      # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-      builtins.concatLists (builtins.genList (
-       	x: let
-       		ws = let
-         		c = (x + 1) / 10;
-         	in
-          	builtins.toString (x + 1 - (c * 10));
-         	in [
-         		"$mod, ${ws}, workspace, ${toString (x + 1)}"
-         		"$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-         	])
-        10)
-      );
+      	# workspaces
+      	# binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+      	builtins.concatLists (builtins.genList (
+       		x: let
+       			ws = let
+         			c = (x + 1) / 10;
+         		in
+          			builtins.toString (x + 1 - (c * 10));
+         			in [
+         				"$mod, ${ws}, workspace, ${toString (x + 1)}"
+         				"$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+         			])
+        		10)
+      		);
 		};
 	};
 }
