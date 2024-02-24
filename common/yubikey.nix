ee444e3adec5df8fbbe9893c803d43d73f19749f
Moxie <astronomicalgamer5@gmail.com>
2024-02-24 01:34:04 -0600

    backlight fixes, again

--- common/yubikey.nix
+++ common/yubikey.nix
@@ -7,6 +7,7 @@
 	programs.gnupg.agent = {
 		enable = true;
 		enableSSHSupport = true;
+		pinentryFlavor = "qt";
 	};
 	environment.shellInit = ''
     	export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
