f2200e43191b1046deb90b2dc0df5e3f41115fd0
Moxie <astronomicalgamer5@gmail.com>
2024-08-05 11:26:00 -0500

    ensure proper formatting disable k3s enable plymouth enable extra gpg socket

--- hw/jbod/disks.nix
+++ hw/jbod/disks.nix
@@ -1,88 +1,86 @@
-{ ... }:
-{
-    # partitions
-    disko.devices = {
-        disk = 
-        let
-        a = {
-            type = "disk"; 
+{...}: {
+  # partitions
+  disko.devices = {
+    disk = let
+      a = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1000M";
+              type = "EF00";
+              content = {
+                type = "mdraid";
+                name = "boot";
+              };
+            };
+            primary = {
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
+            };
+          };
+        };
+      };
+    in
+      {
+        inherit a;
+        disk1 = {device = "/dev/sda";} ++ a;
+        disk2 = {device = "/dev/sdb";} ++ a;
+        disk3 = {device = "/dev/sdc";} ++ a;
+        disk4 = {device = "/dev/sdd";} ++ a;
+      }
+      ++ {
+        mdadm = {
+          boot = {
+            type = "mdadm";
+            level = 1;
+            metadata = "1.0";
             content = {
-                type = "gpt";
-                partitions = {
-                    boot = {
-                        size = "1000M";
-                        type = "EF00";
-                        content = {
-                            type = "mdraid";
-                            name = "boot";
-                        };
-                    };
-                    primary = {
-                        size = "100%";
-                        content = {
-                            type = "lvm_pv";
-                            vg = "pool";
-                        };
-                    };
-                };
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
             };
+          };
         };
-        in
-        {
-            inherit a;
-            disk1 = { device = "/dev/sda"; } ++ a;
-            disk2 = { device = "/dev/sdb"; } ++ a;
-            disk3 = { device = "/dev/sdc"; } ++ a;
-            disk4 = { device = "/dev/sdd"; } ++ a;
-            
-        } ++ {
-            mdadm = {
-                boot = {
-                    type = "mdadm";
-                    level = 1;
-                    metadata = "1.0";
-                    content = {
-                        type = "filesystem";
-                        format = "vfat";
-                        mountpoint = "/boot";
-                    };
+        lvm_vg = {
+          pool = {
+            type = "lvm_vg";
+            lvs = {
+              root = {
+                size = "500G";
+                lvm_type = "raid5";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                  mountOptions = [
+                    "defaults"
+                  ];
                 };
-            };
-            lvm_vg = {
-                pool = {
-                    type = "lvm_vg";
-                    lvs = {
-                        root = {
-                            size = "500G";
-                            lvm_type = "raid5";
-                            content = {
-                                type = "filesystem";
-                                format = "ext4";
-                                mountpoint = "/";
-                                mountOptions = [
-                                    "defaults"
-                                ];
-                            };
-                        };
-                        swap = {
-                            size = "500G";
-                            content = {
-                                type = "swap";
-                                resumeDevice = true;
-                            };
-                        };
-                        home = {
-                            size = "1000G";
-                            lvm_type = "raid0";
-                            content = {
-                                type = "filesystem";
-                                format = "ext4";
-                                mountpoint = "/home";
-                            };
-                        };
-                    };
+              };
+              swap = {
+                size = "500G";
+                content = {
+                  type = "swap";
+                  resumeDevice = true;
                 };
+              };
+              home = {
+                size = "1000G";
+                lvm_type = "raid0";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/home";
+                };
+              };
             };
+          };
         };
-    };
+      };
+  };
 }
--- hw/nixOwO/hardware.nix
+++ hw/nixOwO/hardware.nix
@@ -1,68 +1,70 @@
-{ config, pkgs, ... }:
-
 {
-	powerManagement = {
-		cpuFreqGovernor = "powersave";
-		powertop.enable = true;
-	};
-	services = {
-		libinput.enable = true;
-		xserver = {
-			videoDrivers = [ "amdgpu" "nvidia" ];
-		};
-		udev.extraRules = ''
-			 # Remove NVIDIA USB xHCI Host Controller devices, if present
-    		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+  config,
+  pkgs,
+  ...
+}: {
+  powerManagement = {
+    cpuFreqGovernor = "powersave";
+    powertop.enable = true;
+  };
+  services = {
+    libinput.enable = true;
+    xserver = {
+      videoDrivers = ["amdgpu" "nvidia"];
+    };
+    udev.extraRules = ''
+      # Remove NVIDIA USB xHCI Host Controller devices, if present
+      		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
 
-    		# Remove NVIDIA USB Type-C UCSI devices, if present
-    		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+      		# Remove NVIDIA USB Type-C UCSI devices, if present
+      		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
 
-    		# Remove NVIDIA Audio devices, if present
-    		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+      		# Remove NVIDIA Audio devices, if present
+      		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
 
-    		# Remove NVIDIA VGA/3D controller devices
-    		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-		'';
-	};
+      		# Remove NVIDIA VGA/3D controller devices
+      		ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+    '';
+  };
 
-	hardware = {
-		enableAllFirmware = true;
-		cpu.amd = {
-			updateMicrocode = true;
-			sev.enable = true;
-		};
-		opentabletdriver.enable = true;
-		ksm.enable = true;
+  hardware = {
+    enableAllFirmware = true;
+    cpu.amd = {
+      updateMicrocode = true;
+      sev.enable = true;
+    };
+    opentabletdriver.enable = true;
+    ksm.enable = true;
 
-		opengl = {
-			enable = true;
-			driSupport = true;
-			driSupport32Bit = true;
-			extraPackages = with pkgs; [
-				rocm-opencl-icd
-				rocm-opencl-runtime
-				vaapiVdpau
-			];
-		};
-		nvidia = {
-			open = false;
-			nvidiaSettings = false;
-			modesetting.enable = true;
-			package = config.boot.kernelPackages.nvidiaPackages.beta;
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        rocm-opencl-icd
+        rocm-opencl-runtime
+        vaapiVdpau
+      ];
+    };
+    nvidia = {
+      open = false;
+      nvidiaSettings = false;
+      modesetting.enable = true;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
 
-			prime = {
-				reverseSync.enable = true;
-				offload = {
-					enable = true;
-					enableOffloadCmd = true;
-				};
-				nvidiaBusId = "PCI:1:0:0";
-				amdgpuBusId = "PCI:5:0:0";
-			};
-			powerManagement = {
-				enable = true;
-				finegrained = false;
-			};
-		};
-	};
+      prime = {
+        reverseSync.enable = true;
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        nvidiaBusId = "PCI:1:0:0";
+        amdgpuBusId = "PCI:5:0:0";
+      };
+      powerManagement = {
+        enable = true;
+        finegrained = false;
+      };
+    };
+  };
 }
