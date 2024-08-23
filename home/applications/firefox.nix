b838ac07bfd4f88cb4b2ea62f4b5f8ab75553bdd
Moxie Benavides <moxie@moxiege.com>
2024-08-22 23:15:11 -0500

    undo firefox profile due to quireks i dont know how to fix setup suspend on my desktop

--- home/applications/firefox.nix
+++ home/applications/firefox.nix
@@ -1,40 +1,6 @@
-{...}:
-{
+{pkgs, ...}: {
   programs.firefox = {
     enable = true;
     package = pkgs.firefox-devedition;
-    profiles = let
-      fonts = {
-        "font.blacklist.underline-offset" = "";
-        "gfx.downloadable-fonts.disable-cache" = true;
-        "gfx.font.rendering.graphite.enabled" = false;
-        "gfx.font.rendering.opentype.svg.enabled" = false;
-      };
-    in {
-      moxie = {
-        settings =
-          {
-            "browser.link.open_newwindow" = 1;
-            "browser.link.open_newwindow.disabled_in_fullscreen" = true;
-
-            "browser.tabs.remote.warmup.enabled" = false;
-            "browser.tabs.remote.warmup.maxTabs" = 0;
-
-            "browser.tabs.allowTabDetach" = false;
-            "browser.tabs.closeWindowWithLastTab" = false;
-
-            "privacy.userContext.enabled" = true;
-            "privacy.userContext.ui.enabled" = true;
-            "privacy.userContext.newTabContainerOnLeftClick.enabled" = true;
-
-            "browser.tabs.remote.separatePrivilegedMozillaWebContentProcess" = false;
-            "browser.tabs.remote.separatedMozillaDomains" = "";
-
-            "browser.tabs.remote.subframesPreferUsed" = false;
-          }
-          ++ fonts;
-      };
-    };
   };
 }
-f
