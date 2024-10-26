294d557b6a84f8cf6139a01498a107502efdf209
Moxie Benavides <moxie@moxiege.com>
2024-10-25 21:50:20 -0500

    updated:    - flake.lock    - flake.nix added:  - comic Shanns Mono as the default font  - name and motd to the devshell template  - added default system to the devshell template  - added docs to the devshell template

--- templates/devshell-basic/flake.nix
+++ templates/devshell-basic/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     devshell.url = "github:numtide/devshell";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    mkdocs-flake.url = "github:applicative-systems/mkdocs-flake";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
@@ -14,13 +15,23 @@
   }: {
     imports = [
       inputs.devshell.flakeModule
+      inputs.mkdocs-flake.flakeModule
     ];
     perSystem = {pkgs, ...}: {
+      systems = ["x86_64-linux"];
+
       devshell.default = {
+        name = "generic devshell";
+        motd = ''
+          Welcome to you dev flake\n
+          Checkout ./flake.nix to get started
+        '';
         env = [];
         commands = [];
         packages = [];
       };
+
+      documentation.mkdocs-root = ./docs;
     };
   };
 }
