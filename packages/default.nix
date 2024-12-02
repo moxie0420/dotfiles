737092cdbf76cc2ed56c63a94aa71d740548fabf
Moxie Benavides <moxie@moxiege.com>
2024-12-01 23:29:43 -0600

    breaking:   - all rose-pine packages are now acessed via rose-pine-${package}
    added:
      - rose-pine-qbittorrent

--- packages/default.nix
+++ packages/default.nix
@@ -1,24 +1,17 @@
-{inputs, ...}: {
-  imports = [
-    inputs.flake-parts.flakeModules.easyOverlay
-  ];
+{
   perSystem = {pkgs, ...}: {
-    packages = with pkgs; {
-      comicMono = callPackage ./comicMono.nix {};
-
-      rose-pine = {
-        cursor = callPackage ./rose-pine/cursor.nix {};
-        hyprland = callPackage ./rose-pine/hyprland.nix {};
-        kitty = callPackage ./rose-pine/kitty.nix {};
-        qbittorrent = callPackage ./rose-pine/qbittorrent.nix {};
-        sddm = callPackage ./rose-pine/sddm.nix {};
-        vesktop = callPackage ./rose-pine/vesktop.nix {};
-        wallpapers = callPackage ./rose-pine/wallpapers.nix {};
-        wofi = callPackage ./rose-pine/wofi.nix {};
-        zen = callPackage ./rose-pine/zen.nix {};
-      };
-
-      sgx-software-enable = callPackage ./sgx-software-enable.nix {};
+    packages = {
+      comicMono = pkgs.callPackage ./comicMono.nix {};
+      rose-pine-cursor = pkgs.callPackage ./rose-pine/cursor.nix {};
+      rose-pine-hyprland = pkgs.callPackage ./rose-pine/hyprland.nix {};
+      rose-pine-kitty = pkgs.callPackage ./rose-pine/kitty.nix {};
+      rose-pine-qbittorrent = pkgs.callPackage ./rose-pine/qbittorrent.nix {};
+      rose-pine-sddm = pkgs.callPackage ./rose-pine/sddm.nix {};
+      rose-pine-vesktop = pkgs.callPackage ./rose-pine/vesktop.nix {};
+      rose-pine-wallpapers = pkgs.callPackage ./rose-pine/wallpapers.nix {};
+      rose-pine-wofi = pkgs.callPackage ./rose-pine/wofi.nix {};
+      rose-pine-zen = pkgs.callPackage ./rose-pine/zen.nix {};
+      sgx-software-enable = pkgs.callPackage ./sgx-software-enable.nix {};
     };
   };
 }
--- packages/rose-pine/qbittorrent.nix
+++ packages/rose-pine/qbittorrent.nix
@@ -1,5 +1,5 @@
 {
-  fetchFromGithub,
+  fetchFromGitHub,
   stdenvNoCC,
   ...
 }:
@@ -7,11 +7,11 @@ stdenvNoCC.mkDerivation {
   pname = "qbittorrent";
   version = "1.0.0";
 
-  src = fetchFromGithub {
+  src = fetchFromGitHub {
     owner = "rose-pine";
     repo = "qbittorrent";
     rev = "a0414e45214c89d4a4dcd8ba09ff31e3361fa059";
-    hash = "";
+    hash = "sha256-KZ0TTzajJ4erpDu3IYEJphYouoCVwQSxR+4Qs+PHNkk=";
   };
 
   installPhase = ''
