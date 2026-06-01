d4d422191ed3a5424589c9cd0a76212ac565c12c
Madeline Benavides <moxiebenavides@proton.me>
2026-06-01 15:54:17 -0500

    updated systemd-resolved for new nix config

--- modules/aspects/system/network.nix
+++ modules/aspects/system/network.nix
@@ -72,10 +72,16 @@
 
       services.resolved = {
         enable = true;
-        dnssec = true;
-        domains = ["~."];
-        fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
-        dnsovertls = true;
+
+        settings = {
+          Resolve = {
+            DNSOverTLS = true;
+            DNSSEC = true;
+
+            Domains = ["~."];
+            FallbackDNS = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+          };
+        };
       };
 
       systemd = {
