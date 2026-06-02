8ef055598652d0129d170ed494333a0237fba501
Moxie <astronomicalgamer5@gmail.com>
2026-06-02 18:55:26 -0500

    removed unused substituters

--- modules/aspects/system/nix.nix
+++ modules/aspects/system/nix.nix
@@ -47,18 +47,12 @@
 
       trusted-substituters = [
         "https://nix-community.cachix.org"
-        "https://cache.garnix.io"
-        "https://numtide.cachix.org"
-        "https://attic.xuyh0120.win/lantian"
       ];
 
       substituters = trusted-substituters;
 
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       ];
     };
 
