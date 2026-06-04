0f29a96646741fc3528274fd3541c2e5bddbd661
Madeline Benavides <moxiebenavides@proton.me>
2026-06-04 00:21:03 -0500

    im an idiot
    (re)adding the binary cache for nix-cachyos-kernel cause im and idiot
    and removed it a couple days ago

--- modules/aspects/system/kernel.nix
+++ modules/aspects/system/kernel.nix
@@ -2,12 +2,18 @@
   flake-file.inputs.nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
 
   system.kernel = {
-    cachyos.nixos = {pkgs, ...}: {
-      boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+    cachyos = {
+      nix = {
+        substituters = ["https://attic.xuyh0120.win/lantian"];
+        trusted-public-keys = ["lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="];
+      };
+      nixos = {pkgs, ...}: {
+        boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
 
-      nixpkgs.overlays = [
-        inputs.nix-cachyos-kernel.overlays.pinned
-      ];
+        nixpkgs.overlays = [
+          inputs.nix-cachyos-kernel.overlays.pinned
+        ];
+      };
     };
   };
 }
