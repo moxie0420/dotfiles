9df4e1ea77e04e6d7d760d2078c4e8a1828585e4
Moxie <astronomicalgamer5@gmail.com>
2026-06-07 03:04:43 -0500

    no longer overide substituters

--- modules/aspects/system/nix.nix
+++ modules/aspects/system/nix.nix
@@ -49,8 +49,6 @@
         "https://nix-community.cachix.org"
       ];
 
-      substituters = trusted-substituters;
-
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
