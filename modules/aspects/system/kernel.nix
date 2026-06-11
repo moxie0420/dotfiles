5ea6a25ea479fdb1272761f4e9f6231d4647b718
Moxie <astronomicalgamer5@gmail.com>
2026-06-10 22:37:08 -0500

    moved the cachyos substituter to its own aspect
    its availible at system.kernel.cachyos.substituter

--- modules/aspects/system/kernel.nix
+++ modules/aspects/system/kernel.nix
@@ -1,12 +1,21 @@
-{inputs, ...}: {
+{
+  inputs,
+  system,
+  ...
+}: {
   flake-file.inputs.nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
 
   system.kernel = {
     cachyos = {
-      nix = {
+      substituter.nix = {
         substituters = ["https://attic.xuyh0120.win/lantian"];
         trusted-public-keys = ["lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="];
       };
+
+      includes = [
+        system.kernel.cachyos.substituter
+      ];
+
       nixos = {pkgs, ...}: {
         boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
 
