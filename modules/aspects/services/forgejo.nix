631889c5e322ec22a52a28a4b1d2965588400019
Madeline Benavides <moxiebenavides@proton.me>
2026-06-14 12:57:26 -0500

    ensure admin user

--- modules/aspects/services/forgejo.nix
+++ modules/aspects/services/forgejo.nix
@@ -1,14 +1,37 @@
-{services, ...}: {
+{
+  services,
+  self,
+  lib,
+  ...
+}: {
   services.forgejo = {
     containerized = {
-      nixos = {
+      includes = [
+        services.forgejo.adminSecret
+      ];
+
+      nixos = {config, ...}: {
+        users.users.forgejo = {
+          group = "forgejo";
+          isSystemUser = true;
+        };
+        users.groups.forgejo = {};
+
         containers.forgejo = {
           autoStart = true;
           privateNetwork = true;
+          bindMounts.${config.age.secrets.forgejo-admin-secret.path}.isReadOnly = true;
           hostAddress = "192.168.100.1";
           localAddress = "192.168.100.11";
           config = services.forgejo.nixos;
         };
+      };
+    };
+
+    adminSecret.nixos = {
+      age.secrets.forgejo-admin-secret = {
+        file = "${self}/secrets/forgejo-admin-secret.age";
+        owner = "forgejo";
       };
     };
 
@@ -17,6 +40,19 @@
       srv = cfg.settings.server;
     in {
       networking.firewall.allowedTCPPorts = [22 3000];
+
+      systemd.services.forgejo.preStart = let
+        adminCmd = "${lib.getExe cfg.package} admin user";
+        pwd =
+          if config.boot.isContainer
+          then "/run/agenix/forgejo-admin-secret"
+          else config.age.secrets.forgejo-admin-secret.path;
+        user = "configUser";
+      in ''
+        ${adminCmd} create --admin --email "root@localhost" --username ${user} --password "$(tr -d '\n' < ${pwd})" || true
+        ## uncomment this line to change an admin user which was already created
+        # ${adminCmd} change-password --username ${user} --password "$(tr -d '\n' < ${pwd})" || true
+      '';
 
       services.forgejo = {
         enable = true;
